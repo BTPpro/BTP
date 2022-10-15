204175b73c03833be745f65866044b4783f8f5c1
gaurav-12agrawal <108812877+gaurav-12agrawal@users.noreply.github.com>
2022-10-15 22:15:51 +0530

    initial

---
+++ BTP/src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Login from './Components/Login.js'
+import  './App.module.css';
+
+ import Footer from "./Components/Footer.js";
+import Landingpage from "./Components/Landingpage.js";
+import Landingpagecomp from "./Components/Landingpagecomp";
+
+export default function App() {
+  return (
+ <>
+{/* <Login/> */}
+<Landingpage/>
+</>
+  );
+}
+
+
